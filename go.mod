efc912a183ca6e3a908a8bbda22af0b3ac3826eb
Innokentiy Sokolov <keshon@zoho.com>
2025-09-03 23:52:42 +0300

    simplify env conf code

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 require (
 	github.com/bregydoc/gtranslate v0.0.0-20200913051839-1bd07f6c1fc5
 	github.com/bwmarrin/discordgo v0.29.0
+	github.com/caarlos0/env/v11 v11.3.1
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/zap v1.27.0
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,66 +1,30 @@
-// /pkg/config/config.go
 package config
 
 import (
 	"log"
-	"os"
-	"strings"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found, falling back to system environment variables")
-	}
-}
-
-func Get(key string) string {
-	return os.Getenv(key)
-}
-
 type Config struct {
-	DiscordToken      string
-	StoragePath       string
-	TasksPath         string
-	ProtectedUsers    []string
-	DeveloperID       string
-	InitSlashCommands bool
+	DiscordToken      string   `env:"DISCORD_TOKEN,required"`
+	StoragePath       string   `env:"STORAGE_PATH" envDefault:"./data/datastore.json"`
+	TasksPath         string   `env:"TASKS_PATH,required"`
+	ProtectedUsers    []string `env:"PROTECTED_USERS" envSeparator:","`
+	DeveloperID       string   `env:"DEVELOPER_ID"`
+	InitSlashCommands bool     `env:"INIT_SLASH_COMMANDS" envDefault:"false"`
 }
 
 func New() *Config {
-	var storagePath string
-
-	if Get("STORAGE_PATH") == "" {
-		storagePath = "./data/datastore.json"
-	} else {
-		storagePath = Get("STORAGE_PATH")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, falling back to system environment variables")
 	}
 
-	if Get("DISCORD_TOKEN") == "" {
-		log.Fatal("DISCORD_TOKEN is not set")
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	if Get("TASKS_PATH") == "" {
-		log.Fatal("TASKS_PATH is not set")
-	}
-
-	protectedUsersEnv := Get("PROTECTED_USERS")
-	var protectedUsers []string
-	if protectedUsersEnv != "" {
-		protectedUsers = strings.Split(protectedUsersEnv, ",")
-		for i := range protectedUsers {
-			protectedUsers[i] = strings.TrimSpace(protectedUsers[i])
-		}
-	}
-
-	return &Config{
-		DiscordToken:      Get("DISCORD_TOKEN"),
-		StoragePath:       storagePath,
-		TasksPath:         Get("TASKS_PATH"),
-		ProtectedUsers:    protectedUsers,
-		DeveloperID:       Get("DEVELOPER_ID"),
-		InitSlashCommands: Get("INIT_SLASH_COMMANDS") == "true",
-	}
+	return &cfg
 }
